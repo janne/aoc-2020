09c8f9812b7f132936ebb0dd7261793e8152c528
Jan Andersson <jan.andersson@gmail.com>
2021-01-14 23:50:13 +0000

    Split input

--- src/day4.rs
+++ src/day4.rs
@@ -2,5 +2,10 @@ use std::fs;
 
 pub fn task_a() -> i32 {
     let text = fs::read_to_string("inputs/day4.txt").unwrap();
-    return text.len() as i32;
+    let passports = text.split("\n\n");
+    let passports: Vec<Vec<&str>> = passports
+        .map(|pp| pp.split(|c| c == '\n' || c == ' ').collect::<Vec<&str>>())
+        .collect();
+    println!("First row: {:?}", passports[0]);
+    return passports.len() as i32;
 }
