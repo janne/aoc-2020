a91ef2c2ea11d1d00ebd1e4c73bc81845d49317b
Jan Andersson <jan.andersson@gmail.com>
2021-01-15 00:09:03 +0100

    Import aoc code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate regex;
+
+mod day1;
+mod day2;
+mod day3;
+mod utils;
+
 fn main() {
-    println!("Hello, world!");
+  println!("Day 1A = {}", day1::task_a());
+  println!("Day 1B = {}", day1::task_b());
+  println!("Day 2A = {}", day2::task_a());
+  println!("Day 2B = {}", day2::task_b());
+  println!("Day 3A = {}", day3::task_a());
 }
---
+++ src/utils.rs
@@ -0,0 +1,21 @@
+use regex::Regex;
+use std::fmt::Debug;
+use std::str::FromStr;
+
+pub fn read_input<T>(filename: &str) -> Vec<T>
+where
+  T: FromStr,
+  T::Err: Debug,
+{
+  let data = std::fs::read_to_string(filename).unwrap();
+  let data: Vec<T> = data
+    .lines()
+    .map(|line| line.parse::<T>().unwrap())
+    .collect();
+  data
+}
+
+pub fn instances_of(sub: &str, text: &str) -> i32 {
+  let re = Regex::new(sub).unwrap();
+  re.captures_iter(text).count() as i32
+}
